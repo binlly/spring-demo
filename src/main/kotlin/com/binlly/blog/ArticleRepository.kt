bc0dcd9f236cc0521e9679c582e3f979d399cbdd
yangbin <yangbin@weidian.com>
2018-08-01 20:09:33 +0800

    init

---
+++ src/main/kotlin/com/binlly/blog/ArticleRepository.kt
@@ -0,0 +1,10 @@
+
+package com.binlly.blog
+
+import org.springframework.data.repository.CrudRepository
+
+interface ArticleRepository : CrudRepository<Article, Long> {
+	fun findAllByOrderByAddedAtDesc(): Iterable<Article>
+}
+
+interface UserRepository : CrudRepository<User, String>
